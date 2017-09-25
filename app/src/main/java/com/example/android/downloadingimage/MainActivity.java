5fb3d714d0d69a3b73de292b5f36674a3dac09e0
abdelmobdi hassan <eygptgirl000@yahoo.com>
2017-09-25 14:59:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/downloadingimage/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.android.downloadingimage;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    public void download(View view) {
+        // http://vignette2.wikia.nocookie.net/disney/images/6/6f/Donald_Duck.png/revision/latest?cb=20160508181127
+        ImageDownload image = new ImageDownload();
+        Bitmap myBitmap;
+        try {
+            myBitmap = image.execute("http://vignette2.wikia.nocookie.net/disney/images/6/6f/Donald_Duck.png/revision/latest?cb=20160508181127").get();
+            imageView.setImageBitmap(myBitmap);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+    }
+
+    public class ImageDownload extends AsyncTask<String,Void,Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                Bitmap mybitmap = BitmapFactory.decodeStream(inputStream);
+                return mybitmap;
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+           return null;
+        }
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
